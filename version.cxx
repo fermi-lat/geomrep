61b4f92731213d2f38273ee64c071f1e4ba5af5b
pfkeb <>
2000-02-21 22:52:29 +0000

    Put libgeomrep at top level

---
+++ version.cxx
@@ -0,0 +1,12 @@
+/*
+ * Library version file
+ *
+ * Author: Paul_Kunz@slac.stanford.edu
+ *
+ */
+
+#include "config.h"
+
+static char const package_string[] = PACKAGE;
+static char const version_string[] = VERSION;
+static char const rcsid[] = "$Id: version.cxx,v 1.1 2000/02/15 13:39:59 pfkeb Exp $";

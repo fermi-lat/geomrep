f4db60528340b2e8d7dc1cc276244064ef784e19
burnett <>
2001-01-23 03:07:03 +0000

    belated fix to new interface

--- src/test/testgeom.cxx
+++ src/test/testgeom.cxx
@@ -1,11 +1,11 @@
 //  Simple main to test some features of the GUI interface
 
 #include <string> // make sure gets loaded first
-#include "gui/EventLoop.h"
 #include "gui/CompoundCommand.h"
 #include "gui/GUI.h"
 #include "gui/SimpleCommand.h"
 #include "gui/PrintControl.h"
+#include "gui/GuiMgr.h"
 
 #include "geometry/Box.h"
 #include "geometry/Cone.h"
@@ -16,75 +16,75 @@
 
 # include <strstream>
 
-GUI* theGUI;   // make the theGUI available below
-DisplayControl* g_display;
+gui::DisplayControl* g_display;
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class Lines : public gui::DisplayRep {
 public:
-
-    Lines( const Hep3Vector& p, const Volume& v )
-	: m_p(p), m_v(v){
-		
-	// make a key update the display
-	CompoundCommand* cmd = 
-	    new CompoundCommand(new SimpleCommand<DisplayControl>(g_display, &DisplayControl::clear));
-	cmd->append(new SimpleCommand<DisplayControl>(g_display,&DisplayControl::update));
-	Menu::instance()->register_key('c',cmd);
+    
+    
+    Lines( const gui::GraphicsVector& p, const Volume& v )
+        : m_p(p), m_v(v){
+        using namespace gui;	
+        // make a key update the display
+        CompoundCommand* cmd = 
+            new CompoundCommand(new SimpleCommand<DisplayControl>(g_display, &DisplayControl::clear));
+        cmd->append(new SimpleCommand<DisplayControl>(g_display,&DisplayControl::update));
+        Menu::instance()->register_key('c',cmd);
     }
     
     void draw_a_line(const Vector& n){
-	Point p( m_p.x(),m_p.y(), m_p.z() );
-	
-	if( !m_v.inside(p) ){
-	    // starting outside
-	    setColor("green");
-	    move_to(p);
-	    double d = m_v.distanceToEnter(Ray(p, n), 100);
-	    if (d == FLT_MAX) {
-		// missed, apparently
-		d = 100;
-		line_to(p+d*n);
-		setColor("black");
-		return;
-	    }else {
-		// make a line to instersection
-		Point p2 = p+d*n;
-		line_to(p2);
-		p = p2;
-	    }
-	}
-	// starting, or now inside - move to exit surface
-	setColor("red");
-	move_to(p);  // if not already
-	double d = m_v.distanceToLeave(Ray(p, n), 100);
-	Point p2 = p+d*n;
-	line_to(p2);
-	p = p2;
-	
-	// now outside -- finish up
-	setColor("green");
-	move_to(p);
-	line_to(p+100*n);
-	setColor("black");
+        Point p( m_p.x(),m_p.y(), m_p.z() );
+        
+        if( !m_v.inside(p) ){
+            // starting outside
+            setColor("green");
+            move_to(p);
+            double d = m_v.distanceToEnter(Ray(p, n), 100);
+            if (d == FLT_MAX) {
+                // missed, apparently
+                d = 100;
+                line_to(p+d*n);
+                setColor("black");
+                return;
+            }else {
+                // make a line to instersection
+                Point p2 = p+d*n;
+                line_to(p2);
+                p = p2;
+            }
+        }
+        // starting, or now inside - move to exit surface
+        setColor("red");
+        move_to(p);  // if not already
+        double d = m_v.distanceToLeave(Ray(p, n), 100);
+        Point p2 = p+d*n;
+        line_to(p2);
+        p = p2;
+        
+        // now outside -- finish up
+        setColor("green");
+        move_to(p);
+        line_to(p+100*n);
+        setColor("black");
     }
     
     
     void update() {
-	Vector n(0,0,1);
-	int count = 30;
-	double delta = 2*M_PI/count;
-	
-	for(int i =0; i<count; ++i, n.rotateY(delta)){
-	    draw_a_line(n);
-	}
-	draw_a_line(Vector(0,1,0));
-	draw_a_line(Vector(0,-1,0));
-	
+        Vector n(0,0,1);
+        int count = 30;
+        double delta = 2*M_PI/count;
+        
+        for(int i =0; i<count; ++i, n.rotateY(delta)){
+            draw_a_line(n);
+        }
+        draw_a_line(Vector(0,1,0));
+        draw_a_line(Vector(0,-1,0));
+        
     }
     
 private:
-    const Hep3Vector& m_p;
+    const gui::GraphicsVector& m_p;
     const Volume& m_v;
 };
 
@@ -95,19 +95,13 @@ private:
 int main(int argc, char* argv[])
 {
     
-    // create an instance of a GUI subclass, using the "factory"
-    theGUI = GUI::createGUI("testgeom","test of geometry");
-    // get the Menu object
-    Menu& menu = *Menu::instance();
-    PrintControl printer(theGUI->textWindow("test"));
+    // get pointer to the GuiMgr instance 
+    gui::GuiMgr* guiMgr=gui::GuiMgr::instance();
     
     
-    
-    // create display controller
-    DisplayControl display(menu, theGUI->graphicsWindow());
-    
-    g_display = & display; // global
-        
+    // get the display for below
+    gui::DisplayControl & display = guiMgr->display();
+    g_display = & display;
     
     
     // create object to test
@@ -119,21 +113,17 @@ int main(int argc, char* argv[])
     display.add(new ConeRep(cone), "object to test" );
     display.add(new Lines(display.reference_point(), vol), "lines to object");
     
-    // finally, run it
-    Menu::instance()->run();
+    // start the message loop
+    guiMgr->menu().run();
+    
     return 0;
 }
 //===============================================================
 void WARNING(const char* s){
-    GUI::instance()->inform(s);}
+    gui::GUI::instance()->inform(s);}
 void FATAL(const char* s){
-    GUI::instance()->inform(s);}//cerr << s << endl; exit(-1);}
+    gui::GUI::instance()->inform(s);}//cerr << s << endl; exit(-1);}
 //===============================================================
-#if  defined(WIN32)   
-extern "C" int UserMain(int argc, char *argv[]){
-    return main(argc, argv);
-}
-#endif
 
 
 

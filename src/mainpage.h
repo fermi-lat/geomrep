a58400c296f39b2f24673c256a91957eacb70cfc
burnett <>
2002-01-02 03:02:20 +0000

    remove automake; add mainpage

---
+++ src/mainpage.h
@@ -0,0 +1,15 @@
+//$Header:  $
+// (Special "header" just for doxygen)
+
+/*! @mainpage  geomrep
+
+    This package contains graphical representations for various geometry objects.
+    A test program demonstrates how to set up a simple display, and shows intersections of 
+    rays and shapes.
+
+  <hr>
+  \section requirements requirements
+  \include cmt/requirements
+
+
+*/

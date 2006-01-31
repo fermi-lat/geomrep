3123575a93f938642695b74a2d3db76b73a79494
usher <>
2006-01-31 22:10:13 +0000

    Updates for CLHEP v1.9.2.2

--- geomrep/PointRep.h
+++ geomrep/PointRep.h
@@ -6,24 +6,24 @@
 
 #include "gui/DisplayRep.h"
 
-class Hep3Vector;
+namespace CLHEP {class Hep3Vector;}
 
 class PointRep : public gui::DisplayRep 
 // class that represents a single point in ArveGraphics
 {
 
 public:
-    PointRep(const Hep3Vector& t);
+    PointRep(const CLHEP::Hep3Vector& t);
     // constructor: save reference to given point
 
     virtual void update();
     // create a representation of the point
 
 private:
-    const Hep3Vector & m_point;
+    const CLHEP::Hep3Vector & m_point;
 };
 // Inlines
-inline PointRep::PointRep(const Hep3Vector& t):m_point(t){}
+inline PointRep::PointRep(const CLHEP::Hep3Vector& t):m_point(t){}
 
 #endif // POINTREP_H
 

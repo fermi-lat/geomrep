5dc2a8f6b76c1d0b3090c1c480fb0654452d03f4
jrb <>
2009-08-07 00:51:54 +0000

    use registerTargets put everything in geomrepLib that belongs there add SCons derived stuff to .cvsignore

--- geomrepLib.py
+++ geomrepLib.py
@@ -1,7 +1,8 @@
-#$Header$
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/geomrep/geomrepLib.py,v 1.1 2008/08/26 22:01:44 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['geomrep'])
     env.Tool('geometryLib')
+    env.Tool('guiLib')
 def exists(env):
     return 1;
